b33991e0d72aa061f0b7840ecf9ce8715aa6e21b
Nolan Lawson <nolan@nolanlawson.com>
2021-11-07 09:30:33 -0800

    fix: handle names too long, do not reflect q

--- JNameConverterFunction/src/main/java/jnameconverter/App.java
+++ JNameConverterFunction/src/main/java/jnameconverter/App.java
@@ -22,9 +22,10 @@ import com.google.gson.GsonBuilder;
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    private static Gson gson = new GsonBuilder().create();
-    private static JapaneseNameGenerator japaneseNameGenerator = buildJapaneseNameGenerator();
-    private static JapaneseNameGenerator buildJapaneseNameGenerator() {
+    private static final int MAX_LENGTH = 200;
+    private static final Gson gson = new GsonBuilder().create();
+    private static final JapaneseNameGenerator japaneseNameGenerator = buildJapaneseNameGenerator();
+    private static final JapaneseNameGenerator buildJapaneseNameGenerator() {
         InputStream roomajiInputStream = new BufferedInputStream(
                 App.class.getClassLoader().getResourceAsStream(
                         "roomaji_model_20090128_pop1_3_3_min2_fewer_rules_hacked.txt"));
@@ -36,28 +37,32 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
     private ConversionResult getConversionResult(String query) throws ConversionException {
         Pair<String, String> result = japaneseNameGenerator.convertToRomaajiAndKatakana(query);
-        return new ConversionResult(false, query, result.getFirst(), result.getSecond());
+        return new ConversionResult(false, result.getFirst(), result.getSecond());
     }
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json;charset=utf-8");
-        headers.put("Cache-Control", "public, max-age=604800, s-max-age=604800");
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
         String query = null;
         try {
             query = input.getQueryStringParameters().get("q");
+            int len = query.length();
+            if (len > MAX_LENGTH) {
+                throw new Error("Max length exceeded. Max is " + MAX_LENGTH + " and length is: " + len);
+            }
             ConversionResult conversionResult = this.getConversionResult(query);
             String output = gson.toJson(conversionResult);
 
+            headers.put("Cache-Control", "public, max-age=604800, s-max-age=604800");
             return response
                     .withStatusCode(200)
                     .withBody(output);
         } catch (Throwable t) {
             t.printStackTrace();
-            ConversionResult conversionResult = new ConversionResult(true, query, null, null);
+            ConversionResult conversionResult = new ConversionResult(true, null, null);
             String output = gson.toJson(conversionResult);
             return response
                     .withStatusCode(500)
--- JNameConverterFunction/src/test/java/jnameconverter/AppTest.java
+++ JNameConverterFunction/src/test/java/jnameconverter/AppTest.java
@@ -21,7 +21,7 @@ public class AppTest {
     assertEquals(result.getHeaders().get("Content-Type"), "application/json;charset=utf-8");
     String content = result.getBody();
     assertNotNull(content);
-    assertEquals(content, "{\"error\":false,\"roomaji\":\"nooran\",\"katakana\":\"ノーラン\",\"q\":\"nolan\"}");
+    assertEquals(content, "{\"error\":false,\"roomaji\":\"nooran\",\"katakana\":\"ノーラン\"}");
   }
 
   @Test
@@ -39,6 +39,25 @@ public class AppTest {
   public void noQuery() {
     App app = new App();
     APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent();
+    APIGatewayProxyResponseEvent result = app.handleRequest(input, null);
+    assertEquals(result.getStatusCode().intValue(), 500);
+    assertEquals(result.getHeaders().get("Content-Type"), "application/json;charset=utf-8");
+    String content = result.getBody();
+    assertNotNull(content);
+    assertEquals(content, "{\"error\":true}");
+  }
+
+  @Test
+  public void maxLengthOver() {
+    App app = new App();
+    APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent();
+    input.setQueryStringParameters(new HashMap<String, String>(){{
+      String str = "";
+      for (int i = 0; i < 10000; i++) {
+        str += "foo";
+      }
+      put("q", str);
+    }});
     APIGatewayProxyResponseEvent result = app.handleRequest(input, null);
     assertEquals(result.getStatusCode().intValue(), 500);
     assertEquals(result.getHeaders().get("Content-Type"), "application/json;charset=utf-8");
